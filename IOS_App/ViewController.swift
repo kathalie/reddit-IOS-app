3f77ee4ddaecebda389fe7703f9c036fb06db0f9
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-02-10 22:40:16 +0100

    Initial Commit

---
+++ IOS_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_App
+//
+//  Created by Kathryn Verkhogliad on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
